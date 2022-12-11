0aa535ec1e08802fca4c4c6eb8469952b59cd97b
Jannik Wempe <jannik@wempe.dev>
2022-12-11 18:01:22 +0100

    Initial commit

---
+++ test/trunk-based-gh-action-cdk-example.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TrunkBasedGhActionCdkExample from '../lib/trunk-based-gh-action-cdk-example-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/trunk-based-gh-action-cdk-example-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TrunkBasedGhActionCdkExample.TrunkBasedGhActionCdkExampleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});

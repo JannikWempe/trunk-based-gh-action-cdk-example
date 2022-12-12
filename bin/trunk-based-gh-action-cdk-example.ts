60c13eb94926c74cc5dc657624083000620ddd3f
Jannik Wempe <jannik@wempe.dev>
2022-12-12 07:14:19 +0100

    add prod environment

--- bin/trunk-based-gh-action-cdk-example.ts
+++ bin/trunk-based-gh-action-cdk-example.ts
@@ -2,20 +2,31 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { TrunkBasedGhActionCdkExampleStack } from '../lib/trunk-based-gh-action-cdk-example-stack';
+import { Environment } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+class DeployStage extends cdk.Stage {
+  constructor(scope: Construct, id: string, config: { env: Environment }) {
+    super(scope, id, config);
+
+    new TrunkBasedGhActionCdkExampleStack(app, 'TrunkBasedGhActionCdkExampleStack', {
+      env: config.env,
+    });
+  }
+}
 
 const app = new cdk.App();
-new TrunkBasedGhActionCdkExampleStack(app, 'TrunkBasedGhActionCdkExampleStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+new DeployStage(app, 'Dev', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  }
+});
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new DeployStage(app, 'Prod', {
+  // this should usually be a different account
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  }
 });
